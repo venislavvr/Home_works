c2eb4f332359737a204815c9301658ffe7c0f1b5
venislavvr <venislav.valchev@gmail.com>
2016-02-03 16:27:41 +0200

    add task 5

--- HomeworkPHP2.php
+++ HomeworkPHP2.php
@@ -76,18 +76,66 @@ else{
 //Homework Task 4
 echo "<h2>Homework Task 4</h2>";
 
-$count=100;
-$price;
+$count=rand(0, 1000);
+echo "Единица изразходвана електроенергия: ".$count;
+echo "<br>";
+$first_price=(50*0.10)*0.20;
+$secondary_price=((150*0.15)*0.20)+$first_price;
+$third_price=((250*0.25)*0.20)+$secondary_price;
 
-if ($count<=50) {
-	$price=0.10;
-	$fin=($count*$price)*0.20;
-	echo "цената на електроенергията е ".$fin;
+if ($count>=51 && $count<=150) {
+	$sec_price=(($count*0.15)*0.20)+$first_price;
+	echo "цената на електроенергията е ".$sec_price;
 }
-elseif($count>=51 && $count<=150){
-	$price1=0.15;
-	$fin1=($count*$price1)*0.20;
-	$finalprice=$fin+$fin1;
-	echo "цената на електроенергията е ".$finalprice;
+	elseif($count>=151 && $count<=250){
+	
+	$thirdy_price=(($count*0.25)*0.20)+$secondary_price;
+	echo "цената на електроенергията е ".$thirdy_price;
+}
 
+	elseif($count>=251){
+	$fourth_price=(($count*0.35)*0.20)+$third_price;
+	echo "цената на електроенергията е ".$fourth_price;
+
+}
+else{
+	$fin=($count*0.10)*0.20;
+	echo "цената на електроенергията е ".$fin;
+
+}
+
+echo "<br>";
+
+
+//Homework Task 5
+echo "<h2>Homework Task 5</h2>";
+
+$dd=rand(0, 100);
+$mm=rand(0, 100);
+$YY=rand(1000, 9999);
+echo "<br>";
+echo "денят е: ".$dd;
+echo "<br>";
+echo "месецът е: ".$mm;
+echo "<br>";
+echo "годината е: ".$YY;
+echo "<br>";
+
+if(($dd>=1 && $dd<=31) && ($mm>=1 && $mm<=12) && (strlen ($YY) == 4)){
+	echo "датата е валидна";
+}else {
+	echo "датата не е валидна";
+}
+
+
+
+//Homework Task 6
+echo "<h2>Homework Task 6</h2>";
+
+$a = array(rand(0, 100),rand(0, 100), rand(0, 100),rand(0, 100),rand(0, 100) );
+sort($a);
+$length=count($a);
+for ($i=0; $i < $length; $i++) { 
+	echo $a[$i];
+	echo'<br>';
 }

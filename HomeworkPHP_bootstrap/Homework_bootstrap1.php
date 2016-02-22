cc9a3df6952cfdd3f00b1a5a80275f80533029e7
venislavvr <venislav.valchev@gmail.com>
2016-02-22 23:14:05 +0200

    Added files via upload

---
+++ HomeworkPHP_bootstrap/Homework_bootstrap1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Task1</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php
+
+$n=5;
+$m=5;
+echo "<table class='table table-hover'>";
+for($i=0; $i<$n; $i++){
+	echo "<tr class='success'>";
+		for($j=0; $j<$m; $j++){
+			echo "<td>".$i."-".$j."</td>";
+		}
+
+}
+
+echo "</tr>";
+
+echo "</table>";
+
+?>
+</body>
+</html>
+
